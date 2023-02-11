1f7370f4f43b92e21ac897ff280f22f4cd87bc0e
Maria-Aidarus <maidarus@andrew.cmu.edu>
2023-02-11 09:57:27 +0300

    edited the Employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,17 @@
 class Employee < ApplicationRecord
+    ## Relationships 
+    has_many :assignments
+
+    ## Scopes
+    scope :active, -> { where(active: true) }
+    scope :inactive, -> { where(active: false) }
+    scope :alphabetical, -> { order('last_name, first_name') }
+    scope :is_18_or_older, -> { where('date_of_birth < ?', 18.years.ago) }
+    scope :younger_than_18, -> { where('date_of_birth > ?', 18.years.ago) }
+    # roles | mapping
+    enum :role, { employee: 1, manager: 2, admin: 3 }
+    scope :regulars, -> { where(role: 1) }
+    scope :managers, -> { where(role: 2) }
+    scope :admins, -> { where(role: 3) }
+    
 end
