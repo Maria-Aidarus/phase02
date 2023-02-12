a730c53e2eb833e1506cc260575a82a9b4bf676a
Maria-Aidarus <maidarus@andrew.cmu.edu>
2023-02-12 09:14:54 +0300

    Updates Assignment Model

--- db/migrate/20230211062939_create_assignments.rb
+++ db/migrate/20230211062939_create_assignments.rb
@@ -3,6 +3,8 @@ class CreateAssignments < ActiveRecord::Migration[7.0]
     create_table :assignments do |t|
       t.references :store, null: false, foreign_key: true
       t.references :employee, null: false, foreign_key: true
+      t.date :start_date
+      t.date :end_date
 
       t.timestamps
     end
