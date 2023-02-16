e2b7f7d970ce2e5f6ccbfa6cf13277afe857f049
Maria-Aidarus <maidarus@andrew.cmu.edu>
2023-02-16 15:02:44 +0300

    Added all the scopes for assignmentsrb, creates validations and methods for store.rb

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,20 @@ class Assignment < ApplicationRecord
   ## Relationships 
   belongs_to :store
   belongs_to :employee
+
+  ## Scopes
+  scope :current, -> { where(end_date: nil) }
+  scope :past -> { where.not(end_date: nil) }
+
+  scope :by_store -> { (joins(:store).order('name')) }
+  scope :by_employee, -> { (joins(:employee).order('last_name', 'first_name')) }
+
+  scope :chronological, -> { order('start_date DESC') }
+  scope :for_store, ->(store) { where('store_id = ?', store.id)}
+
+  scope :for_emoloyee, ->(employee) { where('employee_id = ?', employee.id)}
+  scope :for_role, ->(role) { (joins(:employee).where('role = ?', Employee.roles[role])) }
+  scope :for_date, ->(date) { where('start_date <= ? AND (end_date > ? OR end_date IS NULL)', date, date) }
+  
+
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,10 +1,39 @@
 class Store < ApplicationRecord
     ## Relationships
+    #------------------
     has_many :assignments
     has_many :employees, through: :assignments
 
+    ## Validations
+    #------------------
+    # validating the presence of several fields
+    validates_presence _of :zip, :phone, :name, :street, :city, :state
+    # validating the zip code
+    validates_format_of :zip, with: /\A\d{5}\z/, message: "The Zip Code should be 5 digits long."
+    # validating th phone number
+    validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "The Phone Number should be 10 digits long."
+    # validating the store name, must be unique
+
+    # validating the states 
+    validates_inclusion_of :state, in: %w[PA OH WV], message: "State can be either: PA, OH, WV."
+    
+
     ## Scopes
+    #------------------
     scope :active, -> { where(active: true) }
     scope :inactive, -> { where(active: false) }
     scope :alphabetical, -> { order('name') }
+
+    ## Methods
+    #------------------
+    def make_active 
+        self.active = true
+        self.save!
+    end
+
+    def make_inactive
+        self.active = false
+        self.save!
+    end
+
 end
