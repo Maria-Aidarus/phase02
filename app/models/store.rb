1df39962dd3169feee2278a47e114d2d2761bc1a
Maria-Aidarus <maidarus@andrew.cmu.edu>
2023-02-16 15:12:41 +0300

    finished validations for store.rb

--- app/models/store.rb
+++ app/models/store.rb
@@ -8,15 +8,15 @@ class Store < ApplicationRecord
     #------------------
     # validating the presence of several fields
     validates_presence _of :zip, :phone, :name, :street, :city, :state
+    # validating uniqueness of store name
+    validates_uniqueness_of :name
     # validating the zip code
     validates_format_of :zip, with: /\A\d{5}\z/, message: "The Zip Code should be 5 digits long."
     # validating th phone number
     validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "The Phone Number should be 10 digits long."
-    # validating the store name, must be unique
-
     # validating the states 
     validates_inclusion_of :state, in: %w[PA OH WV], message: "State can be either: PA, OH, WV."
-    
+
 
     ## Scopes
     #------------------
@@ -26,14 +26,26 @@ class Store < ApplicationRecord
 
     ## Methods
     #------------------
+    # active == true
     def make_active 
         self.active = true
         self.save!
     end
 
+    # active == false
     def make_inactive
         self.active = false
         self.save!
     end
+
+    # reformats the phone number
+    def reformat_phone
+        # changes the var to a string 
+        phone = self.phone.to_s  
+        # removes all of the non-digit values
+        phone = phone.gsub(/[^0-9]/,"") 
+        # creates new string stored with the phone number
+        self.phone = phone       
+      end
 
 end
