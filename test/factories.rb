f790d9e04651aef16a532db44b95cc0f3a29d332
Maria-Aidarus <maidarus@andrew.cmu.edu>
2023-02-17 20:44:49 +0300

    adding unit tests

--- test/factories.rb
+++ test/factories.rb
@@ -1,2 +1,34 @@
 FactoryBot.define do
+    
+    # Store
+    factory :store do
+        name { "CMU Cafe" }
+        street { "The Street One" }
+        city { "Pittsburgh" }
+        state { "PA" }
+        zip { "12345" }
+        phone { rand(10 ** 10).to_s.rjust(10,'0') }
+        active true
+    end
+
+    # Employee
+    factory :employee do
+        first_name { "Maria" }
+        last_name { "Aidarus" }
+        ssn { "123456789" }
+        date_of_birth { 20.years.ago.to_date }
+        phone  { rand(10 ** 10).to_s.rjust(10,'0') }
+        role { 3 }
+        active true
+    end
+
+    # Assignment
+    factory :assignment do
+        association :store
+        association :employee
+        start_date { 2.years.ago.to_date }
+        end_date { 1.years.ago.to_date }
+    end
+
+
 end
--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -4,4 +4,40 @@ describe Store do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+
+  # Relationships 
+  should have_many(:assignments)
+  should have_many(:employees).through(:assignments)
+
+  # Validations
+  should validate_presence_of(:zip)
+  should validate_presence_of(:phone)
+  should validate_presence_of(:name)
+  should validate_presence_of(:street)
+  should validate_presence_of(:city)
+
+  # validating zip
+  should allow_value("12345").for(:zip)
+  should allow_value("15217").for(:zip)
+  should allow_value("15090").for(:zip)
+
+  should_not allow_value("text").for(:zip)
+  should_not allow_value("1234").for(:zip)
+  should_not allow_value("12h89").for(:zip)
+  should_not allow_value("8921A").for(:zip)
+
+  # validating state
+  should allow_value("PA").for(:state)
+  should allow_value("WV").for(:state)
+  should allow_value("OH").for(:state)
+
+  should_not allow_value("hi").for(:state)
+  should_not allow_value("CA").for(:state)
+  should_not allow_value(100).for(:state)
+  should_not allow_value(10).for(:state)
+
+
+
+  
+
 end
