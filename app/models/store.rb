97647a30c3e68ff0d4dd26d85b4fdc4c15ad289b
Maria-Aidarus <maidarus@andrew.cmu.edu>
2023-02-19 06:45:49 +0300

    finished the store unit test

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,51 +1,52 @@
 class Store < ApplicationRecord
-    ## Relationships
+
+    # Relationships
     #------------------
     has_many :assignments
     has_many :employees, through: :assignments
 
-    ## Validations
+    # Callback
+    before_save :reformat_phone
+
+    # Validations
     #------------------
     # validating the presence of several fields
-    validates_presence _of :zip, :phone, :name, :street, :city, :state
+    validates_presence _of :name, :street, :city, :state, :zip, :phone
     # validating uniqueness of store name
-    validates_uniqueness_of :name
+    validates_uniqueness_of :name, :case_sensitive => false
     # validating the zip code
     validates_format_of :zip, with: /\A\d{5}\z/, message: "The Zip Code should be 5 digits long."
     # validating the phone number
-    validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "The Phone Number should be 10 digits long."
+    validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-. ]\d{4})\z/, message: "The Phone Number should be 10 digits long."
     # validating the states 
     validates_inclusion_of :state, in: %w[PA OH WV], message: "State can be either: PA, OH, WV."
 
-
-    ## Scopes
+    # Scopes
     #------------------
     scope :active, -> { where(active: true) }
     scope :inactive, -> { where(active: false) }
     scope :alphabetical, -> { order('name') }
 
-    ## Methods
-    #------------------
-    # active == true
     def make_active 
         self.active = true
         self.save!
     end
 
-    # active == false
     def make_inactive
         self.active = false
         self.save!
     end
 
-    # reformats the phone number
-    #def reformat_phone
+    # Private Methods
+    #------------------
+    private
+    def reformat_phone
         # changes the var to a string 
-        #phone = self.phone.to_s  
+        phone = self.phone.to_s  
         # removes all of the non-digit values
-        #phone = phone.gsub(/[^0-9]/,"") 
+        phone = phone.gsub(/[^0-9]/,"") 
         # creates new string stored with the phone number
-        #self.phone = phone       
-    #end
+        self.phone = phone       
+    end
 
 end
--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -10,14 +10,17 @@ describe Store do
   should have_many(:employees).through(:assignments)
 
   # Validations
-  should validate_presence_of(:zip)
-  should validate_presence_of(:phone)
   should validate_presence_of(:name)
   should validate_presence_of(:street)
   should validate_presence_of(:city)
+  should validate_presence_of(:state)
+  should validate_presence_of(:zip)
+  should validate_presence_of(:phone)
+  should validate_uniqueness_of(:name).case_insensitive
 
   # validating zip
   should allow_value("12345").for(:zip)
+  should allow_value("23431").for(:zip)
   should allow_value("15217").for(:zip)
   should allow_value("15090").for(:zip)
 
@@ -47,5 +50,45 @@ describe Store do
   should_not allow_value("800-TEX-TEEX").for(:phone)
   should_not allow_value("412/268/3259").for(:phone)
   should_not allow_value("412-2683-259").for(:phone)
+
+  # Creating Contexts
+  context "Creating three stores" do 
+    setup do
+      create_stores
+    end
+
+    teardown do
+      destroy_stores
+    end 
+
+    # Test cases
+    should "have a scope that alphabetizes the stores" do
+      assert_equal ["CMU Cafe", "Coffee Beans", "Flat White", "Starbucks"], Store.alphabetical.map{|s| s.name}
+    end
+
+    should "have a scope that returns tha active stores" do
+      assert_equal ["CMU Cafe", "Coffee Beans", "Starbucks"], Store.active.alphabetical.map{|s| s.name}
+    end
+
+    should "have a scope that returns tha inactive stores" do
+      assert_equal ["Flat White"], Store.inactive.alphabetical.map{|s| s.name}
+    end
+
+    should "show that make_active method works" do
+      @flat_white.make_active
+      assert_equal true, @flat_white.active
+    end
+
+    should "show that make inactive method works" do
+      @cmu_cafe.make_inactive
+      assert_equal false, @cmu_cafe.active
+    end
+
+    should "show that the CMU Cafe's phone number was stripped of non-digits" do
+      assert_equal "7628272973", @cmu_cafe.phone
+    end
+
+  end
+
 
 end
